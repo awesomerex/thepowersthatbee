2dcef98c49151a1c6cb757e8eb50eab71780a6db
Rex Soriano <awesomerex@gmail.com>
2016-01-30 12:02:50 -0500

    create logic for warriors following mouse

--- game.js
+++ game.js
@@ -16,16 +16,44 @@ var manifest = {
 
 var game = new Splat.Game(canvas, manifest);
 
+// var generateWarrior = function(x, y){
+// 	var w = new Splat.Entity(x, y, 10, 10);
+// 	w.color = "red";
+// 	w.attached = false;
+// 	w.followMouse = function(){
+
+// 	};
+// };
+
+var placeOnCircle = function(object, mouse, circle){
+	var theta = Math.atan2(mouse.x-circle.cx, mouse.y-circle.cy);
+	console.log(theta);
+	object.x = circle.cx + circle.r * Math.sin(theta);
+	object.y = circle.cy + circle.r * Math.cos(theta);
+};
+
 function drawEntity(context, drawable){
 	context.fillStyle = drawable.color;
 	context.fillRect(drawable.x, drawable.y, drawable.width, drawable.height);
 }
 
 game.scenes.add("title", new Splat.Scene(canvas, function() {
+	//init
 	var scene =this;
 	scene.player = new Splat.Entity(canvas.width/2, canvas.height/2, 50, 50);
 	scene.player.color = "green";
-
+	scene.player.r = 100;
+	scene.player.cx = scene.player.x + scene.player.width/2;
+	scene.player.cy = scene.player.y + scene.player.height/2;
+	scene.warriors = [];
+	 for(var x = 0; x < 5 ; x++){
+	  	var warrior = new Splat.Entity(Math.floor(Math.random() * canvas.width) +1, 
+									   Math.floor(Math.random() * canvas.height) +1,
+									   10, 10);
+	 	warrior.color ="red";
+	 	scene.warriors.push(warrior);
+	 }
+	//scene.warriors.color = "red";
 }, function() {
 	// simulation
 	var scene = this;
@@ -41,12 +69,20 @@ game.scenes.add("title", new Splat.Scene(canvas, function() {
 	if (game.keyboard.isPressed("down")) {
 		scene.player.y += 1;
 	}
+	console.log(game.mouse);
+	for(var x = 0; x< scene.warriors.length; x++){
+		placeOnCircle(scene.warriors[x], game.mouse, scene.player);
+	}
+
 }, function(context) {
 	// draw
 	var scene = this;
 	context.fillStyle = "#092227";
 	context.fillRect(0, 0, canvas.width, canvas.height);
 	drawEntity(context, scene.player);
+	for(var x = 0; x< scene.warriors.length; x++){
+		drawEntity(context, scene.warriors[x]);
+	}
 }));
 
 game.scenes.switchTo("loading");
