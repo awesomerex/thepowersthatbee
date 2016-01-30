c07ada45e78d147d63dd144104f7d3838a477685
Austin <aread1800@gmail.com>
2016-01-30 00:16:15 -0500

    Added hive and bee counts, hive gives bees

--- game.js
+++ game.js
@@ -23,8 +23,20 @@ function drawEntity(context, drawable){
 
 game.scenes.add("title", new Splat.Scene(canvas, function() {
 	var scene =this;
+    scene.workersAtHive = 0;
+    scene.warriorsAtHive = 0;
+    scene.workersInUse = 0;
+    scene.warriorsInUse = 0;
+    
+    scene.timers.spawnbees = new Splat.Timer();
+    scene.timers.spawnbees.expireMillis = 1000;
+    scene.timers.spawnbees.start();
+    
 	scene.player = new Splat.Entity(canvas.width/2, canvas.height/2, 50, 50);
 	scene.player.color = "green";
+    
+    scene.hive = new Splat.Entity(canvas.width/2, canvas.height-100, 50, 50);
+    scene.hive.color = "red";
 
 }, function() {
 	// simulation
@@ -41,12 +53,32 @@ game.scenes.add("title", new Splat.Scene(canvas, function() {
 	if (game.keyboard.isPressed("down")) {
 		scene.player.y += 1;
 	}
+    
+    if (scene.timers.spawnbees.expired()){
+        scene.workersAtHive++;
+        scene.warriorsAtHive++;
+        scene.timers.spawnbees.reset();
+        scene.timers.spawnbees.start();
+    }
+    
+    if (scene.player.collides(scene.hive)){
+        scene.workersInUse += scene.workersAtHive;
+        scene.warriorsInUse += scene.workersAtHive;
+        scene.workersAtHive = 0;
+        scene.warriorsAtHive = 0;
+    }
 }, function(context) {
 	// draw
 	var scene = this;
 	context.fillStyle = "#092227";
 	context.fillRect(0, 0, canvas.width, canvas.height);
+    drawEntity(context, scene.hive);
 	drawEntity(context, scene.player);
+    
+    context.fillStyle = "#ffffff";
+    context.font = "200px";
+    context.fillText(scene.workersInUse, 100, 100);
+    context.fillText(scene.warriorsInUse, canvas.width - 150, 100);
 }));
 
 game.scenes.switchTo("loading");
