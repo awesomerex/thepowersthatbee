58d67e0f0424e39e812ffec6306b9a7d756f98f7
Rex Soriano <awesomerex@gmail.com>
2016-01-29 23:15:22 -0500

    add moveable block

--- game.js
+++ game.js
@@ -16,25 +16,37 @@ var manifest = {
 
 var game = new Splat.Game(canvas, manifest);
 
-function centerText(context, text, offsetX, offsetY) {
-	var w = context.measureText(text).width;
-	var x = offsetX + (canvas.width / 2) - (w / 2) | 0;
-	var y = offsetY | 0;
-	context.fillText(text, x, y);
+function drawEntity(context, drawable){
+	context.fillStyle = drawable.color;
+	context.fillRect(drawable.x, drawable.y, drawable.width, drawable.height);
 }
 
 game.scenes.add("title", new Splat.Scene(canvas, function() {
-	// initialization
+	var scene =this;
+	scene.player = new Splat.Entity(canvas.width/2, canvas.height/2, 50, 50);
+	scene.player.color = "red";
+
 }, function() {
 	// simulation
+	var scene = this;
+	if (game.keyboard.isPressed("left")) {
+		scene.player.x -= 1;
+	}
+	if (game.keyboard.isPressed("right")) {
+		scene.player.x += 1;
+	}
+	if (game.keyboard.isPressed("up")) {
+		scene.player.y -= 1;
+	}
+	if (game.keyboard.isPressed("down")) {
+		scene.player.y += 1;
+	}
 }, function(context) {
 	// draw
+	var scene = this;
 	context.fillStyle = "#092227";
 	context.fillRect(0, 0, canvas.width, canvas.height);
-
-	context.fillStyle = "#fff";
-	context.font = "25px helvetica";
-	centerText(context, "Blank SplatJS Project", 0, canvas.height / 2 - 13);
+	drawEntity(context, scene.player);
 }));
 
 game.scenes.switchTo("loading");
